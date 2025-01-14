d277d1aff2b1c30aa823b37885a5ddb142f2e5c9
sgdxbc <guangda@u.nus.edu>
2025-01-14 11:12:19 +0800

    Update scripts and post three

--- tools/idea.py
+++ tools/idea.py
@@ -5,13 +5,19 @@ from pathlib import Path
 now = datetime.now(timezone.utc).astimezone()
 header = f"""\
 layout: post
+title:  
 date:   {now.strftime("%Y-%m-%d %H:%M:%S %z")}
-categories: [检查点]
-permalink: /unnamed/checkpoint/:year:month:day:output_ext
+categories: [点子]
+permalink: /unnamed/idea/:year:month:day:output_ext
 """
 
-postdir = Path("_posts") / "checkpoints"
-filename = postdir / f"{now.strftime('%Y-%m-%d')}-.markdown"
-assert not filename.exists()
+postdir = Path("_posts") / "ideas"
+postdir.mkdir(exist_ok=True)
+id = 0
+while True:
+    filename = postdir / f"{now.strftime('%Y-%m-%d')}-{id}.markdown"
+    if not filename.exists():
+        break
+    id += 1
 print("Creating", filename)
 filename.write_text("---\n" + header + "---\n\n")
