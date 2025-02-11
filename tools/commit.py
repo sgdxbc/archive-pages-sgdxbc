0b26cb8821d1dd79f33330dd9fc86450ea5d6df5
sgdxbc <guangda@u.nus.edu>
2025-02-11 13:13:29 +0800

    Update commit script

--- tools/commit.py
+++ tools/commit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from subprocess import run
 
+run("./tools/test.sh", shell=True, check=True)
 run("git add .", shell=True, check=True)
 run("git status", shell=True, check=True)
 message = input('message: ')
